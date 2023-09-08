038116e9b1c534cfe6cbe1225ee5abddd07fe3b3
Dani Wilson <danirae.powell@gmail.com>
2023-09-08 10:41:55 -0600

    add note

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @facade = BendersFacade.new
+    @facade = BendersFacade.new#this is not allowing me to view my site on localhost for some reason :( 
   end
 end
